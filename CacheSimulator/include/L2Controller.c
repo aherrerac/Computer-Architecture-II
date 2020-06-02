2121771206c14bc1d123be11299b2aa627b2ff6a
Andre Herrera <andrehe96@gmail.com>
2020-06-01 21:27:14 -0600

    Update CacheSimulator:

---
+++ CacheSimulator/include/L2Controller.c
@@ -0,0 +1,5 @@
+//
+// Created by aherrerac on 1/6/20.
+//
+
+#include "L2Controller.h"
---
+++ CacheSimulator/include/L2Controller.h
@@ -0,0 +1,8 @@
+//
+// Created by aherrerac on 1/6/20.
+//
+
+#ifndef CACHESIMULATOR_L2CONTROLLER_H
+#define CACHESIMULATOR_L2CONTROLLER_H
+
+#endif //CACHESIMULATOR_L2CONTROLLER_H
